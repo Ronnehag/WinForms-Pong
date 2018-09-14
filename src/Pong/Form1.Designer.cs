a6557175f13a3abf6a7f13ca8ea57e9808f042d7
Fredrik Rönnehag <fredrik.ronnehag@gmail.com>
2018-09-14 11:04:26 +0200

    Initial commit

---
+++ src/Pong/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Pong
+{
+    partial class frm_Pong
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.cpu = new System.Windows.Forms.PictureBox();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.tb_playerScore = new System.Windows.Forms.Label();
+            this.tb_cpuScore = new System.Windows.Forms.Label();
+            this.bgTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cpu)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Yellow;
+            this.player.Location = new System.Drawing.Point(12, 186);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(27, 127);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.White;
+            this.ball.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.ball.Location = new System.Drawing.Point(434, 239);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(27, 26);
+            this.ball.TabIndex = 1;
+            this.ball.TabStop = false;
+            // 
+            // cpu
+            // 
+            this.cpu.BackColor = System.Drawing.Color.Fuchsia;
+            this.cpu.Location = new System.Drawing.Point(883, 186);
+            this.cpu.Name = "cpu";
+            this.cpu.Size = new System.Drawing.Size(27, 127);
+            this.cpu.TabIndex = 2;
+            this.cpu.TabStop = false;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.timertick);
+            // 
+            // tb_playerScore
+            // 
+            this.tb_playerScore.AutoSize = true;
+            this.tb_playerScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_playerScore.ForeColor = System.Drawing.Color.Lime;
+            this.tb_playerScore.Location = new System.Drawing.Point(43, 24);
+            this.tb_playerScore.Name = "tb_playerScore";
+            this.tb_playerScore.Size = new System.Drawing.Size(55, 37);
+            this.tb_playerScore.TabIndex = 3;
+            this.tb_playerScore.Text = "00";
+            // 
+            // tb_cpuScore
+            // 
+            this.tb_cpuScore.AutoSize = true;
+            this.tb_cpuScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_cpuScore.ForeColor = System.Drawing.Color.Red;
+            this.tb_cpuScore.Location = new System.Drawing.Point(817, 24);
+            this.tb_cpuScore.Name = "tb_cpuScore";
+            this.tb_cpuScore.Size = new System.Drawing.Size(55, 37);
+            this.tb_cpuScore.TabIndex = 4;
+            this.tb_cpuScore.Text = "00";
+            // 
+            // bgTimer
+            // 
+            this.bgTimer.Interval = 150;
+            this.bgTimer.Tick += new System.EventHandler(this.bgtimertick);
+            // 
+            // frm_Pong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.ClientSize = new System.Drawing.Size(957, 521);
+            this.Controls.Add(this.tb_cpuScore);
+            this.Controls.Add(this.tb_playerScore);
+            this.Controls.Add(this.cpu);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.player);
+            this.Name = "frm_Pong";
+            this.Text = "Pong";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cpu)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.PictureBox cpu;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label tb_playerScore;
+        private System.Windows.Forms.Label tb_cpuScore;
+        private System.Windows.Forms.Timer bgTimer;
+    }
+}
+
---
+++ src/Pong/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pong
+{
+    public partial class frm_Pong : Form
+    {
+        bool goUp; // Bool för att upptäcka player up position
+        bool goDown; // Bool för att upptäcka player down position
+        int speed = 5; // Värde för hastigheten
+        int ballX = 5; // Värde för bollhastigheten horizontal
+        int ballY = 5; // Värde för bollhastigheten vertical
+        int ball2X = 5; // Värde för bollhastighet horizontal ball 2
+        int ball2Y = 5; // Värde för bollhastighet vertical ball 2
+        int score = 0; // Lagrar spelarens poäng
+        int cpuScore = 0; // Lagrar datorns poäng
+        int bgSwitch = 0; // Bakgrundsfärgen
+
+
+        public frm_Pong()
+        {
+            InitializeComponent();
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Down) goDown = true;
+            if (e.KeyCode == Keys.Up) goUp = true;
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Down) goDown = false;
+            if (e.KeyCode == Keys.Up) goUp = false;
+        }
+
+        
+        private void timertick(object sender, EventArgs e)
+        {
+                    
+            tb_playerScore.Text = "" + score;
+            tb_cpuScore.Text = "" + cpuScore;
+
+            ball.Top -= ballY;
+            ball.Left -= ballX;
+
+            cpu.Top += speed;
+
+
+            if (score < 5)
+            {
+                if (cpu.Top < 0 || cpu.Top > 250)
+                {
+                    speed = -speed;
+                }
+            }
+
+            else cpu.Top = ball.Top + 30;
+
+            if (ball.Left < 0)
+            {
+                ball.Left += 434;
+                ballX -= ballX;
+                ballX -= 2;
+                cpuScore++;
+                
+            }
+            if(ball.Left + ball.Width > ClientSize.Width)
+            {
+                ball.Left = 434;
+                ballX = -ballX;
+                ballX += 2;
+                score++;
+            }
+
+            if (ball.Top < 0 || ball.Top + ball.Height > ClientSize.Height) ballY = -ballY;
+
+            if (ball.Bounds.IntersectsWith(player.Bounds) || ball.Bounds.IntersectsWith(cpu.Bounds)) ballX = -ballX;
+
+            if (goUp == true && player.Top > 0) player.Top -= 8;
+            if (goDown == true && player.Top < 455) player.Top += 8;
+
+            if(score == 10)
+            {
+                gameTimer.Stop();
+                MessageBox.Show($"YOU WON! Final score : {score} - {cpuScore}");
+            }
+
+            if(cpuScore == 10)
+            {
+                gameTimer.Stop();
+                MessageBox.Show($"YOU LOSE! Final score : {score} - {cpuScore}");
+            }
+        }
+
+        private void bgtimertick(object sender, EventArgs e)
+        {
+            bgSwitch++;
+            switch (bgSwitch)
+            {
+                case 1:
+                    BackColor = Color.MistyRose;
+                    break;
+
+                case 2:
+                    BackColor = Color.OrangeRed;
+                    break;
+
+                case 3:
+                    BackColor = Color.Olive;
+                    break;
+
+                case 4:
+                    BackColor = Color.Peru;
+                    break;
+
+                case 5:
+                    BackColor = Color.PowderBlue;
+                    break;
+
+                case 6:
+                    BackColor = Color.SkyBlue;
+                    break;
+
+                default:
+                    break;
+            }
+            if (bgSwitch == 7) bgSwitch = 0;
+
+        }
+    }
+
+}
